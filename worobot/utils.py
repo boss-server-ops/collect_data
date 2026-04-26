b84b9bf09b68699d38df20589398ff5ab4f43550
boss-server-ops <1312136480@qq.com>
2026-04-26 11:05:17 +0800

    fix: lazy-import x1_robot too (same rclpy hard dep)

--- worobot/utils.py
+++ worobot/utils.py
@@ -3,9 +3,17 @@ from pprint import pformat
 
 from .robot import Robot
 from .config import RobotConfig
-from .x1_robot import X1Robot
-from .unix_robot import UnixRobot
-from .piper_robot import PiperRobot
+# x1_robot and unix_robot pull in rclpy (ROS2). For PiperRobot users without
+# ROS2 installed, only piper_robot needs to be importable at module load time.
+try:
+    from .x1_robot import X1Robot  # noqa: F401
+except ImportError:
+    X1Robot = None
+try:
+    from .unix_robot import UnixRobot  # noqa: F401
+except ImportError:
+    UnixRobot = None
+from .piper_robot import PiperRobot  # noqa: F401
 
 def make_robot_from_config(config: RobotConfig) -> Robot:
     if config.type == "unix_robot":
